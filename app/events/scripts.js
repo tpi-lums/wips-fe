288849c18f4822018e609808b15fc433acc64277
Muhammad Haroon <m_haroon96@hotmail.com>
2021-02-11 01:01:17 +0500

    added modal + separated past/upcoming events

--- app/events/scripts.js
+++ app/events/scripts.js
@@ -1,16 +1,15 @@
-const eventsList = new Vue({
-    el: '#eventsList',
-    data: {
-        events: []
-    },
-    created: async function() {
-        let resp = await fetch('{{api}}/getEventList');
-        let res = await resp.json();
-    
-        if (resp.status == 200) {
-            this.events = res;
-            console.log(res);
-        }    
+Vue.component('event-card', {
+    template: '#event-template',
+    props: ['event'],
+    methods: {
+        isSameDay: function(d1, d2) {
+            return moment(d1).isSame(moment(d2), 'day');
+        },
+        imgClick: function(url) {
+            if (url) {
+                this.$emit('img-click', url);
+            }
+        }
     },
     filters: {
         formatDay: function(d) {
@@ -26,9 +25,37 @@ const eventsList = new Vue({
             return moment(d).format('MMM');
         }
     },
+});
+
+const eventsList = new Vue({
+    el: '#eventsList',
+    data: {
+        events: [],
+        modalImageUrl: '',
+        showModal: false
+    },
+    created: async function() {
+        let resp = await fetch('{{api}}/getEventList');
+        let res = await resp.json();
+    
+        if (resp.status == 200) {
+            this.events = res;
+        }    
+    },
+    computed: {
+        pastEvents: function() {
+            return this.events.filter(x => moment().isAfter(x.endsAt));
+        },
+        upcomingEvents: function() {
+            return this.events.filter(x => moment().isBefore(x.endsAt));
+        }
+    },
     methods: {
-        isSameDay: function(d1, d2) {
-            return moment(d1).isSame(moment(d2), 'day');
+        showModalImage: function(url) {
+            this.modalImageUrl = url;
+            this.showModal = true;
         }
     }
 });
+
+
