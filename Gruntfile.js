ac9bb2e9283bad9e7fa5afee5001f82debb0902a
Muhammad Haroon <m_haroon96@hotmail.com>
2021-02-11 01:08:41 +0500

    added css to grunt relativeRoot

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'build/',
-          src: ['**/*.html'],
+          src: ['**/*.html', '**/*.css'],
           dest: 'build/'
         }]
       }
